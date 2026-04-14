85f0e7e7022b680436730212b599fd95c9ce5b31
Anthony Quintero <Anthony2006@github.com>
2026-04-14 13:47:49 +0200

    Añade funciones sumar y restar

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+##calculadora.py
+##Versión inicial
+
+a = int(input("Enter a number: "))
+b = int(input("Enter the second number: "))
+
+def sumar(a, b):
+	return a + b
+
+def restar(a, b):
+	return a - b
+
+func = sumar(a, b)
+print("Resultado:")
+print(func)
